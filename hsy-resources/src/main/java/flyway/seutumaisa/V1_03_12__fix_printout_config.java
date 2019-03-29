d205aa06cd88632cb8d6487d4d08895eff53ea10
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2019-03-29 13:24:02 +0200

    Fixed seutumasa view printout config

---
+++ hsy-resources/src/main/java/flyway/seutumaisa/V1_03_12__fix_printout_config.java
@@ -0,0 +1,29 @@
+package flyway.seutumaisa;
+
+import fi.nls.oskari.domain.map.view.Bundle;
+import fi.nls.oskari.map.layer.OskariLayerService;
+import fi.nls.oskari.map.layer.OskariLayerServiceIbatisImpl;
+import fi.nls.oskari.map.view.ViewService;
+import fi.nls.oskari.map.view.ViewServiceIbatisImpl;
+import fi.nls.oskari.util.FlywayHelper;
+import helpers.LayerHelper;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+import org.json.JSONObject;
+
+import java.sql.Connection;
+
+public class V1_03_12__fix_printout_config implements JdbcMigration {
+    private static final ViewService VIEW_SERVICE = new ViewServiceIbatisImpl();
+    private static final OskariLayerService LAYER_SERVICE = new OskariLayerServiceIbatisImpl();
+
+    private static final String BUNDLE = "printout";
+
+    public void migrate(Connection connection) throws Exception {
+        long viewId = VIEW_SERVICE.getDefaultViewIdForRole(LayerHelper.ROLE_SEUTUMAISA);
+        Bundle bundle = FlywayHelper.getBundleFromView(connection, BUNDLE, viewId);
+        JSONObject config = new JSONObject("{}");
+        bundle.setConfig(config.toString(2));
+        FlywayHelper.updateBundleInView(connection, bundle, viewId);
+
+    }
+}
