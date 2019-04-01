2668d194dacc08d2252dfd45820c9d2dc296161c
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2019-04-01 13:31:45 +0300

    Fixed sql

--- hsy-resources/src/main/resources/flyway/hsy/V1_02_5__fix_layer_kartta_hsy_fi_urls.sql
+++ hsy-resources/src/main/resources/flyway/hsy/V1_02_5__fix_layer_kartta_hsy_fi_urls.sql
@@ -1,9 +1,15 @@
 -- update resources
 UPDATE oskari_resource
 SET resource_mapping = replace(resource_mapping, 'http://kartta.hsy.fi', 'https://kartta.hsy.fi')
-WHERE resource_mapping LIKE '%http://kartta.hsy.fi%';
+WHERE resource_mapping LIKE '%http://kartta.hsy.fi%'
+  AND
+        replace(resource_mapping, 'http://kartta.hsy.fi', 'https://kartta.hsy.fi') NOT IN (SELECT resource_mapping FROM oskari_resource
+                                                                                           WHERE resource_mapping LIKE '%https://kartta.hsy.fi%');
 
 -- update layer urls
 UPDATE oskari_maplayer
-SET url = replace(resource_mapping, 'http://kartta.hsy.fi', 'https://kartta.hsy.fi')
+SET url = replace(url, 'http://kartta.hsy.fi', 'https://kartta.hsy.fi')
 WHERE url LIKE '%http://kartta.hsy.fi%';
+
+-- remove old resources
+DELETE FROM oskari_resource WHERE  resource_mapping LIKE '%http://kartta.hsy.fi%';
