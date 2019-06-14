9e829497de513a1ba788fce6c3a32df026580a61
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2019-06-14 23:34:50 +0300

    Search field from database

---
+++ server-extension/src/main/java/hsy/seutumaisa/Select.java
@@ -0,0 +1,88 @@
+package hsy.seutumaisa;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Select {
+    private String type;
+    private String placeHolderText;
+    private List<SelectValue> values;
+    private String title;
+    private String id;
+
+    public Select() {
+        this.type = "select";
+        this.values = new ArrayList<>();
+        this.placeHolderText = "Valitse";
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPlaceHolderText() {
+        return placeHolderText;
+    }
+
+    public void setPlaceHolderText(String placeHolderText) {
+        this.placeHolderText = placeHolderText;
+    }
+
+    public List<SelectValue> getValues() {
+        return values;
+    }
+
+    public void addValue(SelectValue value) {
+        this.values.add(value);
+    }
+
+    public void setValues(List<SelectValue> values) {
+        this.values = values;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("id", this.id);
+        json.put("title", this.title);
+        json.put("type", this.type);
+        json.put("placeHolderText", this.placeHolderText);
+        JSONArray values = new JSONArray();
+        Collections.sort(this.values, new Comparator<SelectValue>() {
+            public int compare(SelectValue value1, SelectValue value2) {
+                return value1.getTitle().compareTo(value2.getTitle());
+            }
+        });
+        for(SelectValue value : this.values){
+            values.put(value.toJSON());
+        }
+        json.put("values", values);
+        return json;
+    }
+
+}
---
+++ server-extension/src/main/java/hsy/seutumaisa/actions/GetSeutumaisaSearchFieldsHandler.java
@@ -0,0 +1,33 @@
+package hsy.seutumaisa.actions;
+
+
+import fi.nls.oskari.annotation.OskariActionRoute;
+import fi.nls.oskari.control.ActionException;
+import fi.nls.oskari.control.ActionParameters;
+import fi.nls.oskari.control.ActionParamsException;
+import fi.nls.oskari.control.RestActionHandler;
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.util.ResponseHelper;
+import hsy.pipe.helpers.TagPipeHelper;
+import hsy.seutumaisa.SeutumaisaDBHelper;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+@OskariActionRoute("GetSeutumaisaSearchFields")
+public class GetSeutumaisaSearchFieldsHandler extends RestActionHandler {
+    private static Logger LOG = LogFactory.getLogger(GetSeutumaisaSearchFieldsHandler.class);
+
+    @Override
+    public void handleGet(ActionParameters params) throws ActionException {
+        params.requireLoggedInUser();
+
+        try {
+            JSONArray fields = SeutumaisaDBHelper.getSearchFields();
+            ResponseHelper.writeResponse(params, fields);
+        } catch (JSONException e) {
+            LOG.error("Error for getting seutumaisa fields", e);
+            throw new ActionException("Cannot get fields");
+        }
+    }
+}
