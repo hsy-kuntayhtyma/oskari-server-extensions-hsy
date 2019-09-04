af62aad021b123d15569831dd20d6ede434c1f7f
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2019-09-04 15:25:52 +0300

    Add drawtools to seutumaisa view

---
+++ hsy-resources/src/main/java/flyway/seutumaisa/V1_03_18__add_drawtools_to_views.java
@@ -0,0 +1,22 @@
+package flyway.seutumaisa;
+
+import fi.nls.oskari.map.view.ViewService;
+import fi.nls.oskari.map.view.ViewServiceIbatisImpl;
+import fi.nls.oskari.util.FlywayHelper;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+
+import java.sql.Connection;
+
+public class V1_03_18__add_drawtools_to_views implements JdbcMigration {
+
+    private static final ViewService VIEW_SERVICE = new ViewServiceIbatisImpl();
+    private static final  String BUNDLE = "drawtools";
+    private static final String ROLE = "seutumaisa";
+
+    public void migrate(Connection connection) throws Exception {
+        long viewId = VIEW_SERVICE.getDefaultViewIdForRole(ROLE);
+        if(FlywayHelper.getBundleFromView(connection, BUNDLE, viewId) == null) {
+            FlywayHelper.addBundleWithDefaults(connection, viewId, BUNDLE);
+        }
+    }
+}
