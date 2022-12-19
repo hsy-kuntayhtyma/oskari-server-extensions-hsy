196e56822e047932c641fda187bc4e217e597f46
jampukka <janne.z.heikkila@gmail.com>
2022-12-19 11:34:09 +0200

    reformat to use 4 spaces, remove end of line whitepsace

--- server-extension/src/main/java/hsy/pipe/actions/TagPipeActionHandler.java
+++ server-extension/src/main/java/hsy/pipe/actions/TagPipeActionHandler.java
@@ -16,111 +16,111 @@ import fi.nls.oskari.util.ResponseHelper;
 
 @OskariActionRoute("SearchTagPipe")
 public class TagPipeActionHandler extends RestActionHandler {
-	
-	private static final Logger log = LogFactory.getLogger(TagPipeActionHandler.class);
-	
-	protected final static String PARAM_TAG_ID = "tag_id";
-	protected final static String PARAM_TAG_TYPE = "tag_type";
-	protected final static String PARAM_TAG_ADDRESS = "tag_address";
-	protected final static String PARAM_TAG_PIPE_SIZE = "tag_pipe_size";
-	protected final static String PARAM_TAG_LOW_PRESSURE_LEVEL = "tag_low_pressure_level";
-	protected final static String PARAM_TAG_MAX_PRESSURE_LEVEL = "tag_max_pressure_level";
-	protected final static String PARAM_TAG_MAX_WATER_TAKE = "tag_max_water_take";
-	protected final static String PARAM_TAG_MIN_PRESSURE_LEVEL = "tag_min_pressure_level";
-	protected final static String PARAM_TAG_BOTTOM_HEIGHT = "tag_bottom_height";
-	protected final static String PARAM_TAG_LOW_TAG_HEIGHT = "tag_low_tag_height";
-	protected final static String PARAM_TAG_BARRAGE_HEIGHT = "tag_barrage_height";
-	protected final static String PARAM_TAG_GROUND_HEIGHT = "tag_ground_height";
-	protected final static String PARAM_TAG_OTHER_ISSUE = "tag_other_issue";
-	protected final static String PARAM_TAG_GEOJSON = "tag_geojson";
-	//Separate Finnish kiinteistötunnus (real estate ID) into 4 different fields
-	protected final static String PARAM_TAG_MUNICIPALITY = "tag_municipality";
-	protected final static String PARAM_TAG_NEIGHBORHOOD = "tag_neighborhood";
-	protected final static String PARAM_TAG_BLOCK = "tag_block";
-	protected final static String PARAM_TAG_PLOT = "tag_plot";
-     
-     @Override
-     public void handleGet(ActionParameters params) throws ActionException {
-    	 try {
-    		 ResponseHelper.writeResponse(params, TagPipeHelper.getTagPipes(params.getUser(), params.getLocale().getLanguage()));
-    	 } catch (Exception ex){
-    		 log.error(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(ex));    		 
-    		 throw new ActionParamsException("Couldn't get tagpipes");
-    	 }
-     }
-     
-     @Override
-     public void handleDelete(ActionParameters params) throws ActionException {    	 
-    	 // Only admin user
-    	 params.requireAdminUser();
-    	 
-    	 int tagPipeId = params.getRequiredParamInt(PARAM_TAG_ID);
-    	 
-    	 try {
-    		 if(tagPipeId>0) {
-    			 ResponseHelper.writeResponse(params, TagPipeHelper.delete(tagPipeId));
-    		 } else {
-    			 throw new ActionParamsException("Couldn't delete tagpipe");
-    		 }
-    	 } catch (Exception ex){
-    		 log.error(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(ex));    		 
-    		 throw new ActionParamsException("Couldn't delete tagpipe");
-    	 }
-     }
-     
-     @Override
-     public void handlePost(ActionParameters params) throws ActionException {
-    	 // Only admin user
-    	 params.requireAdminUser();
-       	 
-	   	 try {
+
+    private static final Logger log = LogFactory.getLogger(TagPipeActionHandler.class);
+
+    protected final static String PARAM_TAG_ID = "tag_id";
+    protected final static String PARAM_TAG_TYPE = "tag_type";
+    protected final static String PARAM_TAG_ADDRESS = "tag_address";
+    protected final static String PARAM_TAG_PIPE_SIZE = "tag_pipe_size";
+    protected final static String PARAM_TAG_LOW_PRESSURE_LEVEL = "tag_low_pressure_level";
+    protected final static String PARAM_TAG_MAX_PRESSURE_LEVEL = "tag_max_pressure_level";
+    protected final static String PARAM_TAG_MAX_WATER_TAKE = "tag_max_water_take";
+    protected final static String PARAM_TAG_MIN_PRESSURE_LEVEL = "tag_min_pressure_level";
+    protected final static String PARAM_TAG_BOTTOM_HEIGHT = "tag_bottom_height";
+    protected final static String PARAM_TAG_LOW_TAG_HEIGHT = "tag_low_tag_height";
+    protected final static String PARAM_TAG_BARRAGE_HEIGHT = "tag_barrage_height";
+    protected final static String PARAM_TAG_GROUND_HEIGHT = "tag_ground_height";
+    protected final static String PARAM_TAG_OTHER_ISSUE = "tag_other_issue";
+    protected final static String PARAM_TAG_GEOJSON = "tag_geojson";
+    //Separate Finnish kiinteistötunnus (real estate ID) into 4 different fields
+    protected final static String PARAM_TAG_MUNICIPALITY = "tag_municipality";
+    protected final static String PARAM_TAG_NEIGHBORHOOD = "tag_neighborhood";
+    protected final static String PARAM_TAG_BLOCK = "tag_block";
+    protected final static String PARAM_TAG_PLOT = "tag_plot";
+
+    @Override
+    public void handleGet(ActionParameters params) throws ActionException {
+        try {
+            ResponseHelper.writeResponse(params, TagPipeHelper.getTagPipes(params.getUser(), params.getLocale().getLanguage()));
+        } catch (Exception ex){
+            log.error(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(ex));
+            throw new ActionParamsException("Couldn't get tagpipes");
+        }
+    }
+
+    @Override
+    public void handleDelete(ActionParameters params) throws ActionException {
+        // Only admin user
+        params.requireAdminUser();
+
+        int tagPipeId = params.getRequiredParamInt(PARAM_TAG_ID);
+
+        try {
+            if(tagPipeId>0) {
+                ResponseHelper.writeResponse(params, TagPipeHelper.delete(tagPipeId));
+            } else {
+                throw new ActionParamsException("Couldn't delete tagpipe");
+            }
+        } catch (Exception ex){
+            log.error(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(ex));
+            throw new ActionParamsException("Couldn't delete tagpipe");
+        }
+    }
+
+    @Override
+    public void handlePost(ActionParameters params) throws ActionException {
+        // Only admin user
+        params.requireAdminUser();
+
+        try {
             TagPipeConfiguration conf = paramsToTagConfiguration(params);
             // tag_type required on POST / INSERT -- can't be changed with PUT / UPDATE
-	   		conf.setTagType(params.getRequiredParam(PARAM_TAG_TYPE));
-	   		JSONObject response = TagPipeHelper.insert(conf);
-	   		ResponseHelper.writeResponse(params, response);
-	   	 } catch (Exception ex){
-	   		 log.error(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(ex));
-	   		 throw new ActionParamsException("Couldn't insert tagpipe");
-	   	 }
-     }
-     
-     @Override
-     public void handlePut(ActionParameters params) throws ActionException {
-    	 // Only admin user
-    	 params.requireAdminUser();
-   	 
-	   	 try {
-	   		TagPipeConfiguration conf = paramsToTagConfiguration(params);
-            // tag_id (server created) is required on PUT / UPDATE -- can't be set on POST / INSERT
-	        conf.setTagId(params.getRequiredParamInt(PARAM_TAG_ID));
-	   		JSONObject response = TagPipeHelper.update(conf);
-	   		ResponseHelper.writeResponse(params, response);
-	   	 } catch (Exception ex){
-	   		 log.error(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(ex));    		 
-	   		 throw new ActionParamsException("Couldn't update tagpipe");
-	   	 }
-     }
+            conf.setTagType(params.getRequiredParam(PARAM_TAG_TYPE));
+            JSONObject response = TagPipeHelper.insert(conf);
+            ResponseHelper.writeResponse(params, response);
+        } catch (Exception ex){
+            log.error(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(ex));
+            throw new ActionParamsException("Couldn't insert tagpipe");
+        }
+    }
 
-     private TagPipeConfiguration paramsToTagConfiguration(ActionParameters params) throws Exception {
-         TagPipeConfiguration conf = new TagPipeConfiguration();
-         conf.setTagAddress(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_ADDRESS),""));
-         conf.setTagPipeSize(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_PIPE_SIZE), 0));
-         conf.setTagLowPressureLevel(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_LOW_PRESSURE_LEVEL), 0));
-         conf.setTagMaxPressureLevel(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_MAX_PRESSURE_LEVEL), 0));
-         conf.setTagMaxWaterTake(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_MAX_WATER_TAKE), 0));
-         conf.setTagMinPressureLevel(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_MIN_PRESSURE_LEVEL), 0));
-         conf.setTagBottomHeight(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_BOTTOM_HEIGHT), 0));
-         conf.setTagLowTagHeight(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_LOW_TAG_HEIGHT), 0));
-         conf.setTagBarrageHeight(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_BARRAGE_HEIGHT), 0));
-         conf.setTagGroundHeight(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_GROUND_HEIGHT), 0));
-         conf.setTagOtherIssue(ConversionHelper.getString(params.getHttpParam(PARAM_TAG_OTHER_ISSUE),""));
-         conf.setTagGeoJson(new JSONObject(ConversionHelper.getString(params.getHttpParam(PARAM_TAG_GEOJSON),"")));
-         conf.setTagMunicipality(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_MUNICIPALITY),""));
-         conf.setTagNeighborhood(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_NEIGHBORHOOD),""));
-         conf.setTagBlock(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_BLOCK),""));
-         conf.setTagPlot(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_PLOT),""));
-         return conf;
-     }
+    @Override
+    public void handlePut(ActionParameters params) throws ActionException {
+        // Only admin user
+        params.requireAdminUser();
+
+        try {
+            TagPipeConfiguration conf = paramsToTagConfiguration(params);
+            // tag_id (server created) is required on PUT / UPDATE -- can't be set on POST / INSERT
+            conf.setTagId(params.getRequiredParamInt(PARAM_TAG_ID));
+            JSONObject response = TagPipeHelper.update(conf);
+            ResponseHelper.writeResponse(params, response);
+        } catch (Exception ex){
+            log.error(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(ex));
+            throw new ActionParamsException("Couldn't update tagpipe");
+        }
+    }
+
+    private TagPipeConfiguration paramsToTagConfiguration(ActionParameters params) throws Exception {
+        TagPipeConfiguration conf = new TagPipeConfiguration();
+        conf.setTagAddress(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_ADDRESS),""));
+        conf.setTagPipeSize(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_PIPE_SIZE), 0));
+        conf.setTagLowPressureLevel(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_LOW_PRESSURE_LEVEL), 0));
+        conf.setTagMaxPressureLevel(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_MAX_PRESSURE_LEVEL), 0));
+        conf.setTagMaxWaterTake(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_MAX_WATER_TAKE), 0));
+        conf.setTagMinPressureLevel(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_MIN_PRESSURE_LEVEL), 0));
+        conf.setTagBottomHeight(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_BOTTOM_HEIGHT), 0));
+        conf.setTagLowTagHeight(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_LOW_TAG_HEIGHT), 0));
+        conf.setTagBarrageHeight(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_BARRAGE_HEIGHT), 0));
+        conf.setTagGroundHeight(ConversionHelper.getDouble(params.getHttpParam(PARAM_TAG_GROUND_HEIGHT), 0));
+        conf.setTagOtherIssue(ConversionHelper.getString(params.getHttpParam(PARAM_TAG_OTHER_ISSUE),""));
+        conf.setTagGeoJson(new JSONObject(ConversionHelper.getString(params.getHttpParam(PARAM_TAG_GEOJSON),"")));
+        conf.setTagMunicipality(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_MUNICIPALITY),""));
+        conf.setTagNeighborhood(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_NEIGHBORHOOD),""));
+        conf.setTagBlock(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_BLOCK),""));
+        conf.setTagPlot(ConversionHelper.getString(params.getRequiredParam(PARAM_TAG_PLOT),""));
+        return conf;
+    }
 
 }
